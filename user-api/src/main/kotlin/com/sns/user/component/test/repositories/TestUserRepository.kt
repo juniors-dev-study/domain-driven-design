49fb330e1eabeb69b2d873adcd9c88806f2fc253
chanhyeong <chu7825@gmail.com>
2021-10-31 11:01:42 +0900

    [#22] repository 작은 수정

--- user-api/src/main/kotlin/com/sns/user/component/test/repositories/TestUserRepository.kt
+++ user-api/src/main/kotlin/com/sns/user/component/test/repositories/TestUserRepository.kt
@@ -4,6 +4,6 @@ import com.sns.user.component.test.domains.TestUser
 
 interface TestUserRepository {
     fun save(nickName: String): Int
-    fun save(user: TestUser): Int
+    fun save(user: TestUser): TestUser
     fun findByNickName(nickName: String): TestUser
 }
