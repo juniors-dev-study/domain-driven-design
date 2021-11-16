76cc71a77b664c3472b5097015e674d07c770d10
DaYoung <dy1dy2@gmail.com>
2021-11-16 10:13:27 +0900

    Update user-api/src/main/kotlin/com/sns/user/core/config/SwaggerTag.kt
    Co-authored-by: Chanhyeong Cho <chu7825@gmail.com>

--- user-api/src/main/kotlin/com/sns/user/core/config/SwaggerTag.kt
+++ user-api/src/main/kotlin/com/sns/user/core/config/SwaggerTag.kt
@@ -1,7 +1,5 @@
 package com.sns.user.core.config
 
-class SwaggerTag {
-    companion object {
-        const val SIGN_UP: String = "SIGN_UP"
-    }
+object SwaggerTag {
+    const val SIGN_UP: String = "SIGN_UP"
 }
