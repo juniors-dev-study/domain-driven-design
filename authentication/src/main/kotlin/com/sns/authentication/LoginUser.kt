554405cf2e9927f1ecb2fc1c2dd4933a4a5f19f8
youngvly <dy1dy2@gmail.com>
2021-11-26 21:28:55 +0900

    [#31] 코드정리

--- authentication/src/main/kotlin/com/sns/authentication/LoginUser.kt
+++ authentication/src/main/kotlin/com/sns/authentication/LoginUser.kt
@@ -3,7 +3,6 @@ package com.sns.authentication
 import com.sns.authentication.config.Role
 import com.sns.authentication.user.User
 import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 
@@ -25,12 +24,3 @@ class LoginUser(
             if (user == null) throw UsernameNotFoundException("유저 정보를 찾을 수 없습니다.") else LoginUser(user.id, user.name, user.password)
     }
 }
-
-/**
- * LoginUser 인스턴스를 주입받기위한 어노테이션
- */
-@Target(AnnotationTarget.TYPE, AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-@AuthenticationPrincipal
-annotation class CurrentUser
