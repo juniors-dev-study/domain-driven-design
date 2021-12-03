87ff694dd09bc1484bead25274a80f592284136e
Chanhyeong Cho <chu7825@gmail.com>
2021-12-03 19:19:09 +0900

    [#40] Collection converter 추가 (#46)

--- submodules/commons/src/main/kotlin/com/sns/commons/config/ResourceServerSecurityConfig.kt
+++ submodules/commons/src/main/kotlin/com/sns/commons/config/ResourceServerSecurityConfig.kt
@@ -2,6 +2,7 @@ package com.sns.commons.config
 
 import com.sns.commons.oauth.Role
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
