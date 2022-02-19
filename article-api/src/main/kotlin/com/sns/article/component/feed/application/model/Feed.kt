4c746d73035c44e7ac374ee810969022964ac012
hyounglin.jun <hyounglin.jun@navercorp.com>
2022-02-20 00:14:27 +0900

    #55 feed 조회 추가 - feed 모델 네이밍 변경

--- article-api/src/main/kotlin/com/sns/article/component/feed/application/model/Feed.kt
+++ article-api/src/main/kotlin/com/sns/article/component/feed/application/model/Feed.kt
@@ -1,16 +1,8 @@
 package com.sns.article.component.feed
 
-import com.sns.article.component.article.domains.Article
 import com.sns.article.component.article.domains.ArticleId
-import com.sns.article.component.comment.domains.Comment
-import com.sns.article.component.comment.domains.RootType
-import com.sns.article.component.reaction.domains.Reaction
 import com.sns.article.component.reaction.domains.ReactionType
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
 import java.time.Instant
-import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
 
 /**
  * 단순 wrapping Model
@@ -18,35 +10,32 @@ import javax.validation.constraints.NotNull
  * @author Hyounglin Jun (KR19849)
  */
 class Feed(
-    val article: FeedArticle,
-    val comments: List<FeedComment>,
+    val article: Article,
+    val comments: List<Comment>,
 ) {
 
+    inner class Article(
+        val articleId: ArticleId?,
+        val imageUrls: MutableList<String>?,
+        val body: String?,
+        val writerUserId: String,
+        val updatedAt: Instant,
+        val createdAt: Instant,
+        val reaction: Reaction,
+    )
+
+    inner class Comment(
+        var id: Long? = null,
+        var contents: String,
+        val writerId: String,
+        var createdAt: Instant,
+        var updatedAt: Instant,
+        val reaction: Reaction,
+    )
+
+    inner class Reaction(
+        var id: Long?,
+        val type: ReactionType,
+        val userId: String,
+    )
 }
-
-// TODO 이 객체를 어디다 둘지 고민중
-data class FeedArticle(
-    val articleId: ArticleId?,
-    val imageUrls: MutableList<String>?,
-    val body: String?,
-    val writerUserId: String,
-    val updatedAt: Instant,
-    val createdAt: Instant,
-    val reaction: FeedReaction,
-)
-
-// TODO 이 객체를 어디다 둘지 고민중
-data class FeedComment(
-    var id: Long? = null,
-    var contents: String,
-    val writerId: String,
-    var createdAt: Instant,
-    var updatedAt: Instant,
-    val reaction: FeedReaction,
-)
-
-data class FeedReaction(
-    var id: Long?,
-    val type: ReactionType,
-    val userId: String,
-)
