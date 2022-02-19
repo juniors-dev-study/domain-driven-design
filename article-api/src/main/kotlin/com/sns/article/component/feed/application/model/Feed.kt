e1a03ba07a521f57876f9d7146b4c97f5200b050
hyounglin <bearics@gmail.com>
2022-02-20 00:20:58 +0900

    #55 feed 조회 추가 (#69)
    * #55 feed 조회 추가
    - feed 목록 조회 mock api 추가
    
    * #55 feed 조회 추가
    - feed 모델 네이밍 변경
    
    * #55 feed 조회 추가
    - feed 모델 네이밍 변경
    
    Co-authored-by: hyounglin.jun <hyounglin.jun@navercorp.com>

---
+++ article-api/src/main/kotlin/com/sns/article/component/feed/application/model/Feed.kt
@@ -0,0 +1,41 @@
+package com.sns.article.component.feed
+
+import com.sns.article.component.article.domains.ArticleId
+import com.sns.article.component.reaction.domains.ReactionType
+import java.time.Instant
+
+/**
+ * 단순 wrapping Model
+ * 게시물, 댓글, 리액션 조합
+ * @author Hyounglin Jun (KR19849)
+ */
+class Feed(
+    val article: Article,
+    val comments: List<Comment>,
+) {
+
+    inner class Article(
+        val articleId: ArticleId?,
+        val imageUrls: MutableList<String>?,
+        val body: String?,
+        val writerUserId: String,
+        val updatedAt: Instant,
+        val createdAt: Instant,
+        val reaction: Reaction,
+    )
+
+    inner class Comment(
+        var id: Long? = null,
+        var contents: String,
+        val writerId: String,
+        var createdAt: Instant,
+        var updatedAt: Instant,
+        val reaction: Reaction,
+    )
+
+    inner class Reaction(
+        var id: Long?,
+        val type: ReactionType,
+        val userId: String,
+    )
+}
---
+++ article-api/src/main/kotlin/com/sns/article/endpoints/feed/FeedController.kt
@@ -0,0 +1,40 @@
+package com.sns.article.endpoints.feed
+
+import com.sns.article.component.article.application.ArticleCommandService
+import com.sns.article.component.article.application.ArticleQueryService
+import com.sns.article.component.feed.application.FeedQueryService
+import com.sns.article.endpoints.response.ArticlesResponse
+import com.sns.article.endpoints.response.FeedsResponse
+import com.sns.commons.annotation.IsLoginUser
+import com.sns.commons.oauth.LoginUser
+import io.swagger.annotations.ApiOperation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author Hyounglin Jun (KR19849)
+ */
+@RestController
+@RequestMapping("/api")
+class FeedController(
+    val feedQueryService: FeedQueryService,
+) {
+    @ApiOperation("피드 목록 조회")
+    @ApiResponses(
+        value = [
+            ApiResponse(description = "성공", responseCode = "202"),
+            ApiResponse(description = "해당 유저가 없음", responseCode = "409"),
+        ],
+    )
+    @IsLoginUser
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/v1/feeds")
+    fun getFeeds(loginUser: LoginUser): FeedsResponse {
+        return FeedsResponse.create(feedQueryService.getFeeds(loginUserId = loginUser.id))
+    }
+}
