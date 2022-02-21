66519d4591e111952666a9a8af93b2881295acf1
Chanhyeong Cho <chu7825@gmail.com>
2022-02-21 23:20:57 +0900

    [#55] fix test fail

--- article-api/src/main/kotlin/com/sns/article/component/article/repositories/ArticleCrudRepository.kt
+++ article-api/src/main/kotlin/com/sns/article/component/article/repositories/ArticleCrudRepository.kt
@@ -2,6 +2,7 @@ package com.sns.article.component.article.repositories
 
 import com.sns.article.component.article.domains.Article
 import com.sns.article.component.article.domains.ArticleId
+import com.sns.article.component.article.domains.ArticleScope
 import java.time.Instant
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
@@ -13,5 +14,5 @@ import org.springframework.stereotype.Repository
 interface ArticleCrudRepository : CrudRepository<Article, ArticleId> {
     fun findTop100ByWriterUserIdAndUpdatedAtBefore(writerUserId: String, updatedAt: Instant): List<Article>
 
-    fun findTop100ByWriterUserIdInAndUpdatedAtBeforeAndScopeIn(userIds: List<String>, updatedAt: Instant): List<Article>
+    fun findTop100ByWriterUserIdInAndUpdatedAtBeforeAndScopeIn(userIds: List<String>, updatedAt: Instant, scope: Set<ArticleScope>): List<Article>
 }
